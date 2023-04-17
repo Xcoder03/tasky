9f5d5c09ef146bd4b67aee6597bfc5e3a487d22a
Xcoder <chindaebube03@gmail.com>
2023-04-17 15:34:45 +0100

    created display user route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,12 +1,16 @@
 import express  from "express";
 import { validateUser } from "../middlewares/UserValidation.js";
-import { createUser, loginUser } from "../controllers/UserController.js";
+import { createUser, loginUser, displayAllUsers} from "../controllers/UserController.js";
+import { isLogin } from "../middlewares/isLogin.js";
 const userRoutes = express.Router();
 
 
 // post
 userRoutes.post("/createUser", validateUser, createUser)
 userRoutes.post("/login",loginUser)
+
+// get 
+userRoutes.get("",isLogin,displayAllUsers);
 
 
 export default userRoutes;
