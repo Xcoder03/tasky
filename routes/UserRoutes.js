b4152e11b4c379b752ac81bd1565ed3ff8991e1f
Xcoder <chindaebube03@gmail.com>
2023-04-17 15:22:48 +0100

    created login user route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,11 +1,12 @@
 import express  from "express";
 import { validateUser } from "../middlewares/UserValidation.js";
-import { createUser } from "../controllers/UserController.js";
+import { createUser, loginUser } from "../controllers/UserController.js";
 const userRoutes = express.Router();
 
 
 // post
 userRoutes.post("/createUser", validateUser, createUser)
+userRoutes.post("/login",loginUser)
 
 
 export default userRoutes;
