b5f0d7d351a4df3e47a87f481ba7c7a952d737da
Xcoder <chindaebube03@gmail.com>
2023-05-09 18:18:42 +0100

    created function to set header.

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ import {database} from "./config/dbConnect.js"
 dotenv.config();
 database();
 const app = express();
-app.use(express.json())
+app.use(express)
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 
 const PORT = process.env.PORT || 8080;
 app.use("/api/v1/users", userRoutes)
