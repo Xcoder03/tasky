01fb5e9900dcc90db8ba3dd9ad5503c3d4711154
Xcoder <chindaebube03@gmail.com>
2023-06-16 17:08:14 +0100

    checkCompleted task controller method working

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -1,11 +1,12 @@
 import express  from "express";
-import { createTask, fetchAllTask, deleteTask } from "../controllers/TaskController.js";
+import { createTask, fetchAllTask, deleteTask, checkCompletedTask } from "../controllers/TaskController.js";
 import { isLogin } from "../middlewares/isLogin.js";
 
 const TaskRoutes = express.Router();
  
 TaskRoutes.post("/createTask",isLogin, createTask);
 TaskRoutes.get("/user/:id",isLogin, fetchAllTask);
+TaskRoutes.get("/user/completedTask/:id", isLogin, checkCompletedTask);
 TaskRoutes.delete("/user/:id", isLogin, deleteTask);
 
 export default TaskRoutes;
